40d86466fb8bf60c57777bcb55c82228fdfd26de
renatoelho <renatoelho@gmail.com>
2023-06-26 06:14:17 -0300

    Update repo 20230626040547...

--- app.py
+++ app.py
@@ -15,37 +15,3 @@ if __name__ == "__main__":
         else:
             print("Ocorreu algum erro ao baixar o arquivo...")
             print(teste)
-
-
-
-from urllib.parse import urlsplit
-
-url = "https://www.example.com/path/page.html?query=123"
-
-parsed_url = urlsplit(url)
-base_url = parsed_url.scheme + "://" + parsed_url.netloc
-
-print(base_url)
-
-
-from urllib.parse import urljoin
-
-base_url = "https://www.example.com/path/"
-relative_url = "../page.html"
-
-absolute_url = urljoin(base_url, relative_url)
-print(absolute_url)
-
-
-from urllib.parse import urlparse
-
-def is_url_relativa(url):
-    parsed_url = urlparse(url)
-    return not bool(parsed_url.scheme)
-
-# Exemplos de URLs
-url_absoluta = "https://www.example.com/path/page.html"
-url_relativa = "/path/page.html"
-
-print(is_url_relativa(url_absoluta))  # False
-print(is_url_relativa(url_relativa))  # True
